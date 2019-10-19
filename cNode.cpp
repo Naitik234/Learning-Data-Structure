a57442c31a6de49be29f87ce8f40ca5587940f3c
dawoodmalhi <dawoodmalhi007@gmail.com>
2019-10-19 03:37:50 -0700

    cNode class defined

--- cNode.cpp
+++ cNode.cpp
@@ -1,11 +1,23 @@
 #include "cNode.h"
 
-
-
 cNode::cNode()
 {
 }
 
+cNode::cNode(int d):data(d){
+}
+
+cNode::int getData(){
+			return data;
+		}
+
+cNode::cNode* setData(int data){
+        	this->data=data;
+		}
+		
+cNode::void print()const{
+			cout<<data<<" ";
+		}
 
 cNode::~cNode()
 {
--- cNode.h
+++ cNode.h
@@ -1,8 +1,14 @@
 #pragma once
 class cNode
 {
+	int data;
 public:
+	cNode *next;
 	cNode();
+	cNode(int d);
+	int getData();
+	cNode* setData(int data);
+	void print()const;
 	~cNode();
 };
 
